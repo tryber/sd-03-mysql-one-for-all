4c07164052c1564f628e070415fb82ce2adbdf84
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-07-28 19:10:15 -0300

    Desafio 11 + CC

--- desafio10.sql
+++ desafio10.sql
@@ -5,9 +5,9 @@ DELIMITER $$
 CREATE FUNCTION quantidade_musicas_no_historico (user_id INT)
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE quant INT;
-    SELECT COUNT(*) FROM historico WHERE usuario_id = user_id INTO quant;
-    RETURN quant;
+DECLARE quant INT;
+SELECT COUNT(*) FROM historico WHERE usuario_id = user_id INTO quant;
+RETURN quant;
 END $$
 
 DELIMITER ;
--- desafio9.sql
+++ desafio9.sql
@@ -4,10 +4,10 @@ DELIMITER $$
 
 CREATE PROCEDURE albuns_do_artista (IN nome_artista VARCHAR(100))
 BEGIN
-	SELECT nome_artista AS 'artista', album.nome AS 'album'
-    FROM album AS album
-    INNER JOIN artista AS artista ON artista.id = album.artista_id
-    AND artista.nome = nome_artista;
+SELECT nome_artista AS 'artista', album.nome AS 'album'
+FROM album AS album
+INNER JOIN artista AS artista ON artista.id = album.artista_id
+AND artista.nome = nome_artista;
 END $$
 
 DELIMITER ;
