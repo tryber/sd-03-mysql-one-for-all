f76d178ffc6ebd3acadf584e1823229b37508129
Hfreitas <hfreitas86@gmail.com>
2020-07-28 20:54:43 -0300

    desafio 6

---
+++ desafio6.sql
@@ -0,0 +1,11 @@
+USE SpotifyClone;
+
+CREATE VIEW faturamento_atual AS
+SELECT
+ROUND(MIN(p.plan_value), 2) AS faturamento_minimo,
+ROUND(MAX(p.plan_value), 2) AS faturamento_maximo,
+ROUND(AVG(p.plan_value), 2) AS faturamento_medio,
+ROUND(SUM(p.plan_value), 2) AS faturamento_total
+FROM plans p
+INNER JOIN users us
+ON p.plan_id = us.plan_id;
