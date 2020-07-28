df042b402bf49ab22e850762f2b781c5cb310be0
Hfreitas <hfreitas86@gmail.com>
2020-07-28 20:41:52 -0300

    desafio 3 refatorado

--- desafio3.sql
+++ desafio3.sql
@@ -1,15 +1,7 @@
 USE SpotifyClone;
 CREATE VIEW historico_reproducao_usuarios AS
-SELECT (
-    SELECT users.user
-    FROM users
-    WHERE users.user_id = song_history.user_id;
-) AS usuario,
-(
-  SELECT songs.song
-  FROM songs
-  WHERE songs.song_id = songs_history.song_id
-) AS nome
+SELECT
+(SELECT users.user FROM users WHERE users.user_id = songs_history.user_id) AS usuario,
+(SELECT songs.song FROM songs WHERE songs.song_id = songs_history.song_id) AS nome
 FROM songs_history
-ORDER BY usuario,
-  nome;
+ORDER BY usuario, nome;
