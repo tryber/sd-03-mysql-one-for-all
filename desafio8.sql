a885d9717ade6c63b41e3b640dbd6da941f133ab
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-07-28 19:33:51 -0300

    Correção Desafio 8

--- desafio8.sql
+++ desafio8.sql
@@ -3,7 +3,7 @@ USE SpotifyClone;
 DELIMITER $$
 
 CREATE TRIGGER trigger_usuario_delete
-AFTER DELETE ON usuario
+BEFORE DELETE ON usuario
 FOR EACH ROW
 BEGIN
     DELETE FROM historico WHERE usuario_id = OLD.id;
