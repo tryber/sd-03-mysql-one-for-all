69da2a8b791c857903b504c6008012d49641a49e
Hfreitas <hfreitas86@gmail.com>
2020-07-28 19:18:38 -0300

    tentando mais uma vez

--- desafio1.sql
+++ desafio1.sql
@@ -4,121 +4,135 @@ CREATE DATABASE SpotifyClone;
 
 USE SpotifyClone;
 
-CREATE TABLE plans(
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(60) NOT NULL,
-  plan_value DECIMAL(5, 2) NOT NULL
-) ENGINE = INNODB;
+CREATE TABLE plans (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    plan_value DOUBLE NOT NULL
+)  ENGINE=INNODB;
 
-CREATE TABLE artists(
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(100) NOT NULL
-) ENGINE = INNODB;
+CREATE TABLE artists (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL
+)  ENGINE=INNODB;
 
-CREATE TABLE albums(
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(100) NOT NULL,
-  artist_id INT NOT NULL,
-  FOREIGN KEY(artist_id) REFERENCES artists(id),
-) ENGINE = INNODB;
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    age INT NOT NULL,
+    plan_id INT NOT NULL,
+    FOREIGN KEY (plan_id)
+        REFERENCES plans (id)
+)  ENGINE=INNODB;
 
-CREATE TABLE users(
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(100) NOT NULL,
-  age INT NOT NULL,
-  plan_id INT NOT NULL,
-  FOREIGN KEY(plan_id) REFERENCES plans(id)
-) ENGINE = INNODB;
+CREATE TABLE artists_followers (
+    user_id INT NOT NULL,
+    artist_id INT NOT NULL,
+    FOREIGN KEY (user_id)
+        REFERENCES users (id),
+    FOREIGN KEY (artist_id)
+        REFERENCES artists (id),
+    PRIMARY KEY (user_id , artist_id)
+)  ENGINE=INNODB;
 
-CREATE TABLE songs(
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(100) NOT NULL,
-  album_id INT NOT NULL,
-  FOREIGN KEY(album_id) REFERENCES albums(id),
-) ENGINE = INNODB;
+CREATE TABLE albums (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    artist_id INT NOT NULL,
+    FOREIGN KEY (artist_id)
+        REFERENCES artists (id)
+)  ENGINE=INNODB;
 
-CREATE TABLE artists_followers(
-  user_id INT NOT NULL,
-  artist_id INT NOT NULL,
-  FOREIGN KEY(user_id) REFERENCES users(id),
-  FOREIGN KEY(artist_id) REFERENCES artists(id),
-  PRIMARY KEY (user_id, artist_id)
-) ENGINE = INNODB;
+CREATE TABLE songs (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    album_id INT NOT NULL,
+    FOREIGN KEY (album_id)
+        REFERENCES albums (id)
+)  ENGINE=INNODB;
 
-CREATE TABLE playlist_history(
-  user_id INT NOT NULL,
-  song_id INT NOT NULL,
-  FOREIGN KEY(user_id) REFERENCES users(id),
-  FOREIGN KEY(song_id) REFERENCES songs(id),
-  PRIMARY KEY (user_id, song_id),
-) ENGINE = INNODB;
+CREATE TABLE songs_history (
+    user_id INT NOT NULL,
+    song_id INT NOT NULL,
+    FOREIGN KEY (user_id)
+        REFERENCES users (id),
+    FOREIGN KEY (song_id)
+        REFERENCES songs (id),
+    PRIMARY KEY (user_id , song_id)
+)  ENGINE=INNODB;
 
-INSERT INTO plans(name, plan_value)
-VALUES ('gratuito', 0),
-  ('universitário', 5.99),
-  ('familiar', 7.99);
+INSERT INTO plans (name, plan_value)
+VALUES
+('gratuito', 0),
+('familiar', 7.99),
+('universitário', 5.99);
 
-INSERT INTO artists(name)
-VALUES ('Walter Phoenix'),
-  ('Peter Strong'),
-  ('Lance Day'),
-  ('Freedie Shannon');
-
-INSERT INTO albums(name, artist_id)
-VALUES ('Envious', 1),
-  ('Exuberant', 1),
-  ('Hallowed Steam', 2),
-  ('Incandescent', 3),
-  ('Temporary Culture', 4);
-
-INSERT INTO users(name, age, plan_id)
-VALUES ('Thati', 23, 1),
-  ('Cintia', 35, 3),
-  ('Bill', 20, 2),
-  ('Roger', 45, 1);
-
-INSERT INTO songs(name, album_id)
-VALUES ('Soul For Us', 1),
-  ('Reflections of Magic', 1),
-  ('Dance With Her Own', 1),
-  ('Troubles Of My Inner Fire', 2),
-  ('Time Fireworks', 2),
-  ('Magic Circus', 3),
-  ('Honey, So Do I', 3),
-  (`Sweetie, Let's Go Wild`, 3),
-  ('She Knows', 3),
-  ('Fantasy For Me', 4),
-  ('Celebration Of More', 4),
-  ('Rock His Everything', 4),
-  ('Home Forever', 4),
-  ('Diamond Power', 4),
-  (`Honey, Let's Be Silly`, 4),
-  ('Thang Of Thunder', 5),
-  ('Words Of Her Life', 5),
-  ('Without My Streets', 5);
-
-INSERT INTO artists_followers(user_id, artist_id)
-VALUES (1, 1),
-  (1, 4),
-  (1, 3),
-  (2, 1),
-  (2, 3),
-  (3, 2),
-  (3, 1),
-  (4, 4);
+INSERT INTO artists (name)
+VALUES
+('Walter Phoenix'),
+('Peter Strong'),
+('Lance Day'),
+('Freedie Shannon');
   
-INSERT INTO playlist_history(user_id, song_id)
-VALUES (1, 1),
-  (1, 6),
-  (1, 14),
-  (1, 16),
-  (2, 13),
-  (2, 17),
-  (2, 2),
-  (2, 15),
-  (3, 4),
-  (3, 16),
-  (3, 6),
-  (4, 3),
-  (4, 18),
-  (4, 11);
+INSERT INTO users (name, age, plan_id)
+VALUES
+('Thati', 23, 1),
+('Cintia', 35, 2),
+('Bill', 20, 3),
+('Roger', 45, 1);
+
+INSERT INTO artists_followers (user_id, artist_id)
+VALUES
+(1, 1),
+(1, 4),
+(1,	3),
+(2,	1),
+(2,	3),
+(3,	2),
+(3,	1),
+(4,	4);
+
+INSERT INTO albums (name, artist_id)
+VALUES
+('Envious',	1),
+('Exuberant', 1),
+('Hallowed Steam', 2),
+('Incandescent', 3),
+('Temporary Culture', 4);
+
+INSERT INTO songs (name, album_id)
+VALUES
+('Soul For Us', 1),
+('Reflections Of Magic', 1),
+('Dance With Her Own', 1),
+('Troubles Of My Inner Fire', 2),
+('Time Fireworks', 2),
+('Magic Circus', 3),
+('Honey, So Do I', 3),
+("Sweetie, Let's Go Wild", 3),
+('She Knows', 3),
+('Fantasy For Me', 4),
+('Celebration Of More', 4),
+('Rock His Everything', 4),
+('Home Forever', 4),
+('Diamond Power', 4),
+("Honey, Let's Be Silly", 4),
+('Thang Of Thunder', 5),
+('Words Of Her Life', 5),
+('Without My Streets', 5);
+
+INSERT INTO songs_history (user_id, song_id)
+VALUES
+(1, 1),
+(1, 6),
+(1, 14),
+(1, 16),
+(2, 13),
+(2, 17),
+(2, 2),
+(2, 15),
+(3, 4),
+(3, 16),
+(3, 6),
+(4, 3),
+(4, 18),
+(4, 11);
