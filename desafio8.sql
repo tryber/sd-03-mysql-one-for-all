45550c679e664163f9b5d5e4a96a968a87b2cbb9
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-07-28 18:44:39 -0300

    Correção Desafio 8

--- desafio8.sql
+++ desafio8.sql
@@ -6,8 +6,8 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON usuario
 FOR EACH ROW
 BEGIN
-    DELETE FROM historico WHERE usuario_id = OLD.id
-    DELETE FROM seguindo_artista WHERE usuario_id = OLD.id
-END;
+    DELETE FROM historico WHERE usuario_id = OLD.id;
+    DELETE FROM seguindo_artista WHERE usuario_id = OLD.id;
+END $$
 
-DELIMITER $$ ;
+DELIMITER ;
