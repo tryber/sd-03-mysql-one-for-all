ff83a36bc26aab92befb376ec639a0f6a8f0b839
Gustaff <gustavohmfigueiredo@gmail.com>
2020-07-28 20:32:28 -0300

    10.sql with user id not user name

--- desafio10.sql
+++ desafio10.sql
@@ -1,15 +1,15 @@
 DELIMITER $$
-CREATE FUNCTION quantidade_musicas_no_historico(username VARCHAR(30))
+CREATE FUNCTION quantidade_musicas_no_historico(userId VARCHAR(30))
 RETURNS INT READS SQL DATA
 BEGIN
   DECLARE qnt_of_musics INT;
   SELECT COUNT(*) FROM SpotifyClone.user AS user
   INNER JOIN SpotifyClone.reproduction AS repr
   ON user.id = repr.user_id
-  WHERE user.name = username
+  WHERE user.id = userId
   INTO qnt_of_musics;
   RETURN qnt_of_musics;
 END $$
 DELIMITER ;
 
-SELECT quantidade_musicas_no_historico('Bill');
+SELECT quantidade_musicas_no_historico(3);
