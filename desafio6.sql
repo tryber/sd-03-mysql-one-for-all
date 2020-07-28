48fb9e9502d76ed0d7ff344a60a70beee01d4e29
rodrigo-militao <rodrigo_militao@hotmail.com.br>
2020-07-28 18:11:15 -0300

    challenge 6 missing words

--- desafio6.sql
+++ desafio6.sql
@@ -2,7 +2,7 @@ CREATE VIEW faturamento_atual AS
     SELECT 
         ROUND(MIN(plano.value_plan), 2) AS 'faturamento_minimo',
         ROUND(MAX(plano.value_plan), 2) AS 'faturamento_maximo',
-        ROUND(AVG(plano.value_plan), 2) 'faturamento_medio',
+        ROUND(AVG(plano.value_plan), 2) AS 'faturamento_medio',
         ROUND(SUM(plano.value_plan), 2) AS 'faturamento_total'
     FROM
         SpotifyClone.plan AS plano
