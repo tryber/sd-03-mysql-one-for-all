2fd6b225887a20b6079a5bce3f515fcd151fa7ae
potew <potew0@gmail.com>
2020-07-28 18:49:26 -0300

    Desafios 2 e 3
    E rewamp da 1

--- desafio1.sql
+++ desafio1.sql
@@ -2,54 +2,74 @@ DROP DATABASE IF EXISTS SpotifyClone;
 CREATE DATABASE SpotifyClone;
 USE SpotifyClone;
 
-CREATE TABLE users(
-	id INT PRIMARY KEY AUTO_INCREMENT,
-    user_name VARCHAR(30),
-    user_age INT(2),
-    plan_id INT(2) REFERENCES subscrition_plans.id
+CREATE TABLE subscrition_plans(
+id INT PRIMARY KEY AUTO_INCREMENT,
+plan_name VARCHAR(15) NOT NULL,
+plan_price DECIMAL(5,2) NOT NULL
 ) engine = InnoDB;
 
-CREATE TABLE subscrition_plans(
-	id INT PRIMARY KEY AUTO_INCREMENT,
-    plan_name VARCHAR(15),
-    plan_price DECIMAL(5,2)
+CREATE TABLE users(
+id INT PRIMARY KEY AUTO_INCREMENT,
+user_name VARCHAR(30) NOT NULL,
+user_age INT(2) NOT NULL,
+plan_id INT(2) NOT NULL,
+FOREIGN KEY (plan_id) REFERENCES subscrition_plans(id)
 ) engine = InnoDB;
 
 CREATE TABLE artists(
-	id INT PRIMARY KEY AUTO_INCREMENT,
-    artist_name VARCHAR(30)
+id INT PRIMARY KEY AUTO_INCREMENT,
+artist_name VARCHAR(30) NOT NULL
 ) engine = InnoDB;
 
 CREATE TABLE albums(
-	id INT PRIMARY KEY AUTO_INCREMENT,
-    album_name VARCHAR(30),
-    artist_id INT(2) REFERENCES artists.id
+id INT PRIMARY KEY AUTO_INCREMENT,
+album_name VARCHAR(30) NOT NULL,
+artist_id INT(2),
+FOREIGN KEY (artist_id) REFERENCES artists(id)
 ) engine = InnoDB;
 
 CREATE TABLE songs(
-	id INT PRIMARY KEY AUTO_INCREMENT,
-    song_name VARCHAR(40),
-    album_id INT(2) REFERENCES albums.id
+id INT PRIMARY KEY AUTO_INCREMENT,
+song_name VARCHAR(40) NOT NULL,
+album_id INT(2),
+FOREIGN KEY (album_id) REFERENCES albums(id)
 ) engine = InnoDB;
 
 CREATE TABLE track_history(
-	id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT(2) REFERENCES users.id,
-    song_id INT(2) REFERENCES songs.id
+id INT PRIMARY KEY AUTO_INCREMENT,
+user_id INT(2) NOT NULL,
+song_id INT(2) NOT NULL,
+FOREIGN KEY (user_id) REFERENCES users(id),
+FOREIGN KEY (song_id) REFERENCES songs(id)
 ) engine = InnoDB;
 
 CREATE TABLE followers(
-	id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT(2) REFERENCES users.id,
-    artist_id INT(2) REFERENCES artists.id
+id INT PRIMARY KEY AUTO_INCREMENT,
+user_id INT(2) NOT NULL,
+artist_id INT(2) NOT NULL,
+FOREIGN KEY (user_id) REFERENCES users(id),
+FOREIGN KEY (artist_id) REFERENCES artists(id)
 ) engine = InnoDB;
 
+INSERT INTO subscrition_plans (id, plan_name, plan_price) 
+VALUES
+(1, 'gratuito', 0),
+(2, 'familiar', 7.99),
+(3, 'universitário', 5.99);
+
 INSERT INTO users (id, user_name, user_age, plan_id) 
-VALUES (1, 'Thati', 23, 1),
-	(2, 'Cintia', '35', 2),
-	(3, 'Bill', '20', 3),
-	(4, 'Roger', '45', 1);
-    
+VALUES 
+  (1, 'Thati', 23, 1),
+  (2, 'Cintia', '35', 2),
+  (3, 'Bill', '20', 3),
+  (4, 'Roger', '45', 1);
+
+INSERT INTO artists (id, artist_name) VALUES
+(1, 'Walter Phoenix'),
+(2, 'Freddie Shannon'),
+(3, 'Lance Day'),
+(4, 'Peter Strong');
+
 INSERT INTO albums (id, album_name, artist_id)
 VALUES 
   (1, 'Envious', 1),
@@ -60,36 +80,24 @@ VALUES
 
 INSERT INTO songs (id, song_name, album_id)
 VALUES
-(1, 'Soul for Us', 1),
-(2, 'Magic Circus', 3),
-(3, 'Diamond Power', 4),
-(4, 'Thang of Thunder', 5),
-(5, 'Home Forever', 4),
-(6, 'Words of Her Life', 5),
-(7, 'Reflections Of Magic', 1),
-(8, 'Honey, So Do I', 3),
-(9, 'Troubles Of My Inner Fire', 2),
-(10, 'Dance With Her Own', 1),
-(11, 'Without My Streets', 4),
-(12, 'Celebration of More', 4),
-(13, 'Fantasy For me', 4),
-(14, 'Time Fireworks', 2),
-(15, 'Sweetie, Let\'s Go Wild', 3),
-(16, 'She Knows', 3),
-(17, 'Rock His Everything', 4),
-(18, 'Honey, Let\'s Be Silly', 4);
-
-INSERT INTO subscrition_plans (id, plan_name, plan_price) 
-VALUES
-(1, 'gratuito', 0),
-(2, 'familiar', 7.99),
-(3, 'universitário', 5.99);
-
-INSERT INTO artists (id, artist_name) VALUES
-(1, 'Walter Phoenix'),
-(2, 'Freddie Shannon'),
-(3, 'Lance Day'),
-(4, 'Peter Strong');
+  (1, 'Soul for Us', 1),
+  (2, 'Magic Circus', 3),
+  (3, 'Diamond Power', 4),
+  (4, 'Thang of Thunder', 5),
+  (5, 'Home Forever', 4),
+  (6, 'Words of Her Life', 5),
+  (7, 'Reflections Of Magic', 1),
+  (8, 'Honey, So Do I', 3),
+  (9, 'Troubles Of My Inner Fire', 2),
+  (10, 'Dance With Her Own', 1),
+  (11, 'Without My Streets', 4),
+  (12, 'Celebration of More', 4),
+  (13, 'Fantasy For me', 4),
+  (14, 'Time Fireworks', 2),
+  (15, 'Sweetie, Let\'s Go Wild', 3),
+  (16, 'She Knows', 3),
+  (17, 'Rock His Everything', 4),
+  (18, 'Honey, Let\'s Be Silly', 4);
 
 INSERT INTO track_history (id, user_id, song_id) 
 VALUES 
---
+++ desafio3.sql
@@ -0,0 +1,12 @@
+-- 3. Crie uma VIEW chamada historico_reproducao_usuarios. Essa VIEW deverá ter apenas duas colunas:
+
+-- A primeira coluna deve possuir o alias "usuario" e exibir o nome da pessoa usuária.
+-- A segunda coluna deve possuir o alias "nome" e exibir o nome da canção ouvida pela pessoa com base no seu histórico de reprodução.
+
+-- Os resultados devem estar ordenados por nome da pessoa usuária em ordem alfabética e em caso de empate no nome os resultados devem ser ordenados pelo nome da canção em ordem alfabética.
+
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT U.user_name AS usuario, S.song_name AS nome FROM track_history as H
+INNER JOIN users AS U ON U.id = H.user_id
+INNER JOIN songs AS S ON S.id = H.song_id
+ORDER BY usuario, nome;
