66af9b46f552b3f0cab170b6861829ed61d30200
Hfreitas <hfreitas86@gmail.com>
2020-07-28 21:37:43 -0300

    7 e 8

---
+++ desafio8.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+
+CREATE TRIGGER trigger_usuario_delete
+AFTER DELETE on SpotifyClone.users FOR EACH ROW BEGIN
+DELETE FROM SpotifyClone.artists_followers af
+WHERE af.user_id = OLD.user_id;
+DELETE FROM SpotifyClone.songs_history sh
+WHERE sh.user_id = OLD.user_id;
+END $$
+
+DELIMITER;
