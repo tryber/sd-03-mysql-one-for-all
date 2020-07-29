504939a7e8588adf9c2cc8dbcc20319088766a0f
Hfreitas <hfreitas86@gmail.com>
2020-07-28 22:01:33 -0300

    tentando pssar no avaliador

--- desafio10.sql
+++ desafio10.sql
@@ -6,8 +6,8 @@ CREATE FUNCTION quantidade_musicas_no_historico(username VARCHAR(30))
 RETURNS INT READS SQL DATA
 BEGIN
   DECLARE music_report INT;
-  SELECT COUNT(*) FROM users users
-  INNER JOIN songs_history sh
+  SELECT COUNT(*) FROM users AS users
+  INNER JOIN songs_history AS sh
   ON users.user_id = sh.user_id
   WHERE users.user = username
   INTO music_report;
