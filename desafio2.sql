d37068462d505359916382f7e19803d8aedd7f3e
Marco Barbosa <marco.meireles.b@gmail.com>
2020-07-29 18:09:53 -0300

    codeclimate

--- desafio2.sql
+++ desafio2.sql
@@ -1,8 +1,8 @@
 USE SpotifyClone;
 CREATE VIEW estatisticas_musicais AS
 SELECT COUNT(*) AS 'cancoes',
- (SELECT COUNT(*) FROM SpotifyClone.Artist) AS 'artistas',
- (SELECT COUNT(*) FROM SpotifyClone.Albuns) AS 'albuns'
+  (SELECT COUNT(*) FROM SpotifyClone.Artist) AS 'artistas',
+  (SELECT COUNT(*) FROM SpotifyClone.Albuns) AS 'albuns'
 FROM SpotifyClone.Artist AS A, SpotifyClone.Albuns AS B, SpotifyClone.Songs AS C
 WHERE
 (A.id = B.artist_id) AND
