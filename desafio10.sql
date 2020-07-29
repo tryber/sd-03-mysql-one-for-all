ccca98a716bd883dbd658c15da568ae60755c164
Hfreitas <hfreitas86@gmail.com>
2020-07-28 22:12:29 -0300

    mantendo paridade com tabela users

--- desafio10.sql
+++ desafio10.sql
@@ -2,14 +2,14 @@ USE SpotifyClone;
 
 DELIMITER $$
 
-CREATE FUNCTION quantidade_musicas_no_historico(username VARCHAR(30))
+CREATE FUNCTION quantidade_musicas_no_historico(name VARCHAR(100))
 RETURNS INT READS SQL DATA
 BEGIN
   DECLARE music_report INT;
   SELECT COUNT(*) FROM users AS users
   INNER JOIN songs_history AS sh
   ON users.user_id = sh.user_id
-  WHERE users.user = username
+  WHERE users.user = name
   INTO music_report;
   RETURN music_report;
 END $$
