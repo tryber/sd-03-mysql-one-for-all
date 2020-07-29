0a2523af406fc745fbef4d38c26592814ac7bacf
William Igor <williamigorsilva@gmail.com>
2020-07-29 19:49:24 -0300

    json 1

---
+++ desafio7.sql
@@ -0,0 +1,9 @@
+CREATE VIEW perfil_artistas AS
+SELECT 
+CONCAT(A.primeiro_nome, ' ', A.segundo_nome) AS artista,
+CONCAT(AL.primeiro_nome, ' ', AL.segundo_nome) AS album,
+COUNT(SE.id_artista) AS seguidores FROM SpotifyClone.artista AS A
+INNER JOIN SpotifyClone.albuns AS AL ON AL.artista_id_artista = A.id_artista
+INNER JOIN SpotifyClone.seguindo_artistas AS SE ON SE.id_artista = A.id_artista
+group by AL.id_albun
+ORDER BY seguidores DESC, artista, album;
