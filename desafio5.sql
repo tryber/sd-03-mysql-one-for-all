6c5531ae8e24ace1a913684bc0ab4e7666a98f19
Lucas Werner <lucas.1werner@gmail.com>
2020-07-29 16:27:32 -0300

    desafio 5

---
+++ desafio5.sql
@@ -0,0 +1,11 @@
+CREATE VIEW top_2_hits_do_momento AS
+    SELECT 
+        cancoes.name AS 'cancao',
+        COUNT(historico.song_id) 'reproducoes'
+    FROM
+        SpotifyClone.history AS historico
+            INNER JOIN
+        SpotifyClone.songs AS cancoes ON historico.song_id = cancoes.id
+    GROUP BY (historico.song_id)
+    ORDER BY 2 DESC , 1
+    LIMIT 2;
