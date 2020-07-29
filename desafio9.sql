05ad0ff47d8171f2e0784cf834c3050ce9a5d308
Hfreitas <hfreitas86@gmail.com>
2020-07-28 22:09:48 -0300

    acrescentando filtro

--- desafio9.sql
+++ desafio9.sql
@@ -1,13 +1,14 @@
-DELIMITER $$
-
-CREATE PROCEDURE albuns_ do_artista (IN artist_name VARCHAR(100))
+DELIMITER $$ 
+CREATE PROCEDURE albuns_ do_artista (IN artist_name VARCHAR(100)) 
 BEGIN
-SELECT artist_name AS artista,
-  albums.album AS album
-FROM SpotifyClone.albums AS albums
-  INNER JOIN SpotifyClone.artists AS artists ON artists.artist = artist_name;
-END $$
+  SELECT artist_name AS artista,
+    albums.album AS album
+  FROM SpotifyClone.albums AS albums
+  INNER JOIN SpotifyClone.artists AS artists
+  ON artists.artist_id = albums.artist_id
+  WHERE artists.artist = artist_name;
+END $$ 
 
-DELIMITER ;
+DELIMITER;
 
 CALL albuns_do_artista('Walter Phoenix');
