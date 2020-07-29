b5677381658f74c27c0830afc1493c574853d484
Hfreitas <hfreitas86@gmail.com>
2020-07-28 22:00:50 -0300

    tentando passar no avaliador

--- desafio8.sql
+++ desafio8.sql
@@ -2,9 +2,9 @@ DELIMITER $$
 
 CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE on SpotifyClone.users FOR EACH ROW BEGIN
-DELETE FROM SpotifyClone.artists_followers af
+DELETE FROM SpotifyClone.artists_followers AS af
 WHERE af.user_id = OLD.user_id;
-DELETE FROM SpotifyClone.songs_history sh
+DELETE FROM SpotifyClone.songs_history AS sh
 WHERE sh.user_id = OLD.user_id;
 END $$
 
--- desafio9.sql
+++ desafio9.sql
@@ -1,13 +1,11 @@
-USE SpotifyClone;
-
 DELIMITER $$
 
 CREATE PROCEDURE albuns_ do_artista (IN artist_name VARCHAR(100))
 BEGIN
 SELECT artist_name AS artista,
   albums.album AS album
-FROM albums albums
-  INNER JOIN artists artists ON artists.artist = artist_name;
+FROM SpotifyClone.albums AS albums
+  INNER JOIN SpotifyClone.artists AS artists ON artists.artist = artist_name;
 END $$
 
 DELIMITER ;
