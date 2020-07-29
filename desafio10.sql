f3db4bed7b86f1f105858d7b6719652b9adbd93d
Hfreitas <hfreitas86@gmail.com>
2020-07-28 22:36:33 -0300

    voltando a sintaxe anterior

--- desafio10.sql
+++ desafio10.sql
@@ -2,12 +2,14 @@ USE SpotifyClone;
 
 DELIMITER $$
 
-CREATE FUNCTION quantidade_musicas_no_historico(user_id INT)
+CREATE FUNCTION quantidade_musicas_no_historico(user_name VARCHAR(100))
 RETURNS INT READS SQL DATA
 BEGIN
   DECLARE music_report INT;
-  SELECT COUNT(*) FROM songs_history
-  WHERE user_id = user_id
+  SELECT COUNT(*) FROM users AS users
+  INNER JOIN songs_history AS sh
+  ON users.user_id = sh.user_id
+  WHERE users.user = user_name
   INTO music_report;
   RETURN music_report;
 END $$
