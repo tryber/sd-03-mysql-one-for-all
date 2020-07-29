f51cced24212211ba143bdb2c40d6a9683f905c6
Hfreitas <hfreitas86@gmail.com>
2020-07-28 21:14:01 -0300

    retirando comando use

--- desafio6.sql
+++ desafio6.sql
@@ -1,11 +1,7 @@
-USE SpotifyClone;
-
 CREATE VIEW faturamento_atual AS
-SELECT
-ROUND(MIN(p.plan_value), 2) AS faturamento_minimo,
-ROUND(MAX(p.plan_value), 2) AS faturamento_maximo,
-ROUND(AVG(p.plan_value), 2) AS faturamento_medio,
-ROUND(SUM(p.plan_value), 2) AS faturamento_total
+SELECT ROUND(MIN(p.plan_value), 2) AS faturamento_minimo,
+  ROUND(MAX(p.plan_value), 2) AS faturamento_maximo,
+  ROUND(AVG(p.plan_value), 2) AS faturamento_medio,
+  ROUND(SUM(p.plan_value), 2) AS faturamento_total
 FROM plans p
-INNER JOIN users us
-ON p.plan_id = us.plan_id;
+  INNER JOIN users us ON p.plan_id = us.plan_id;
