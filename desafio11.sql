ce219dd9246ae79eb30a0f7fbc0543285c75da6c
Hfreitas <hfreitas86@gmail.com>
2020-07-28 22:20:46 -0300

    refazendo aliases

--- desafio11.sql
+++ desafio11.sql
@@ -1,15 +1,15 @@
 CREATE VIEW cancoes_premium AS
 SELECT songs.song AS nome,
   COUNT(*) AS reproducoes
-FROM SpotifyClone.songs songs
-  INNER JOIN SpotifyClone.songs_history sh ON sh.song_id = songs.song_id
+FROM SpotifyClone.songs_history AS sh
+  INNER JOIN SpotifyClone.songs AS songs ON sh.song_id = songs.song_id
 WHERE EXISTS (
     SELECT *
-    FROM SpotifyClone.users users
+    FROM SpotifyClone.users AS users
     WHERE users.user_id = sh.user_id
       AND users.plan_id IN (
         SELECT plans.plan_id
-        FROM SpotifyClone.plans plans
+        FROM SpotifyClone.plans AS plans
         WHERE plans.plan = 'familiar'
           OR plans.plan = 'universitário'
       )
