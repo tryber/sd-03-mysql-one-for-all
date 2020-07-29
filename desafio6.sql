0f701ff53b4d1f24bd6f31ee4c3a8285236a87ba
Lucas Werner <lucas.1werner@gmail.com>
2020-07-29 19:30:47 -0300

    mais alguns desafios

---
+++ desafio6.sql
@@ -0,0 +1,11 @@
+CREATE VIEW faturamento_atual AS
+    SELECT 
+        MIN(plano.value_plan) AS 'faturamento_minimo',
+        MAX(plano.value_plan) AS 'faturamento_maximo',
+        ROUND(AVG(plano.value_plan), 2) AS 'faturamento_medio',
+        SUM(plano.value_plan) AS 'faturamento_total'
+    FROM
+        SpotifyClone.plan AS plano
+            INNER JOIN
+        SpotifyClone.user AS usuario ON usuario.plan_id = plano.id;
+        
---
+++ desafio7.sql
@@ -0,0 +1,13 @@
+CREATE VIEW perfil_artistas AS
+    SELECT 
+        artista.name AS 'artista',
+        album.name AS 'album',
+        COUNT(seguindo_artista.artist_id) AS 'seguidores'
+    FROM
+        SpotifyClone.artist AS artista
+            INNER JOIN
+        SpotifyClone.album AS album ON album.artist_id = artista.id
+            INNER JOIN
+        SpotifyClone.following_artist AS seguindo_artista ON seguindo_artista.artist_id = artist.id
+    GROUP BY (album.id)
+    ORDER BY 3 DESC , 1 , 2;
