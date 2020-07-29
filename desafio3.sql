c47558c848fac8b570cb31fbe6ee28e005a6d1ff
Marco Barbosa <marco.meireles.b@gmail.com>
2020-07-29 19:46:25 -0300

    desafio3

---
+++ desafio3.sql
@@ -0,0 +1,11 @@
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT
+U.name AS `usuario`,
+S.title AS `nome`
+FROM
+SpotifyClone.Users AS U
+INNER JOIN Reproduction_History AS H
+ON H.user_id = U.id
+INNER JOIN SpotifyClone.Songs AS S
+ON S.id = H.song_id
+ORDER BY `usuario`, `nome`;
