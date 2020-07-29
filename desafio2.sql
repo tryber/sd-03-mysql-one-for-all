eb3bcd7e0f63e60d5374f6144d651a124e0bdca3
Marco Barbosa <marco.meireles.b@gmail.com>
2020-07-29 16:06:08 -0300

    desafio2

---
+++ desafio2.sql
@@ -0,0 +1,6 @@
+USE SpotifyClone;
+CREATE VIEW estatisticas_musicais AS
+SELECT COUNT(*) AS 'cancoes',
+ (SELECT COUNT(*) FROM SpotifyClone.Artist) AS 'artistas',
+ (SELECT COUNT(*) FROM SpotifyClone.Albuns) AS 'albuns'
+FROM SpotifyClone.Songs;
