165595049bc1fcfd2cf938393cd4f4e2734a8758
clayton Pereira <cpereiramt@gmail.com>
2020-07-30 17:41:23 -0400

    Salvando alterações no desafio 07

---
+++ desafio7.sql
@@ -0,0 +1,11 @@
+  CREATE VIEW `perfil_artistas` 
+   AS SELECT a.artista_name AS `artista`,
+al.album_name AS `album`, 
+COUNT(sa.artista_id) As `seguidores`
+FROM seguindo_artista sa
+INNER JOIN artista a
+ON a.artista_id = sa.artista_id
+INNER JOIN album AS al
+ON a.artista_id = al.artista_id
+GROUP BY `album`
+ORDER BY 1 DESC, 2 ASC
