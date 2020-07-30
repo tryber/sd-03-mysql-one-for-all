913b8466856d22bb5e03e3a4c323b2d94f0bbefe
clayton Pereira <cpereiramt@gmail.com>
2020-07-30 19:35:11 -0400

    Salvando alterações no desafio 03

---
+++ desafio3.sql
@@ -0,0 +1,7 @@
+CREATE VIEW `historico_reproducao_usuarios`
+AS SELECT   us.usuario, ca.cancoes_name AS 'nome'
+FROM historico_reproducao hr
+INNER JOIN  usuarios us
+ON us.usuario_id = hr.usuario_id
+INNER JOIN cancoes ca
+ON ca.cancoes_id = hr.cancoes_id
