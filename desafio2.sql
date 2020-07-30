220e473154961a263945fe63505775bde4f980fb
clayton Pereira <cpereiramt@gmail.com>
2020-07-30 19:12:03 -0400

    Salvando alterações no desafio 02

--- desafio2.sql
+++ desafio2.sql
@@ -2,4 +2,4 @@ CREATE VIEW `estatisticas_musicais`
 AS SELECT COUNT(*) AS 'cancoes' ,
 (SELECT COUNT(*) FROM artista) AS 'artistas',
 (SELECT COUNT(*) FROM album) AS 'album'
-FROM spotifyclone.cancoes
+FROM cancoes
