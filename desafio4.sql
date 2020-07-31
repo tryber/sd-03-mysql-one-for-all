3a3f97e4cd17a42fe25a9816cd4526a148fd9696
clayton Pereira <cpereiramt@gmail.com>
2020-07-30 20:27:45 -0400

    Salvando alterações no desafio 04

--- desafio4.sql
+++ desafio4.sql
@@ -1,6 +1,6 @@
 CREATE VIEW `top_3_artistas` 
-AS SELECT a.artista_name, 
-COUNT(sa.artista_id) As `Seguidores`
+AS SELECT a.artista_name AS `artista`, 
+COUNT(sa.artista_id) As `seguidores`
 FROM seguindo_artista sa
 INNER JOIN artista a
 ON a.artista_id = sa.artista_id
