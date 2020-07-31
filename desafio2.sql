a3ae8fd26797e4a7867beb03a00033a516c51430
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-07-30 21:21:07 -0300

    lal' git

--- desafio2.sql
+++ desafio2.sql
@@ -1,9 +1,9 @@
 CREATE VIEW estatisticas_musicais AS SELECT COUNT(*) AS cancoes,
   (
     SELECT COUNT(*)
-	FROM SpotifyClone.artistas
+    FROM SpotifyClone.artistas
   ) AS artistas,
   (
-	SELECT COUNT(*) FROM SpotifyClone.albums
+    SELECT COUNT(*) FROM SpotifyClone.albums
   ) AS albuns
 FROM SpotifyClone.cancoes can;
--- desafio6.sql
+++ desafio6.sql
@@ -3,5 +3,4 @@ MAX(plan.valor_plano) AS 'faturamento_maximo',
 ROUND(AVG(plan.valor_plano), 2) AS 'faturamento_medio',
 SUM(plan.valor_plano) AS 'faturamento_total'
 FROM SpotifyClone.planos plan, SpotifyClone.usuarios userss
-where plan.plano_id = userss.plano_id ;
-
+where plan.plano_id = userss.plano_id;
