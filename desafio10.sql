b6ec993606d219da8d20c8b88d57a879f018bcf0
Gabriel <dreadpool38@gmail.com>
2020-07-31 19:50:47 -0300

    fix

--- desafio10.sql
+++ desafio10.sql
@@ -1,14 +1,13 @@
-USE SpotifyClone;
 DELIMITER $$
 
-CREATE FUNCTION quantidade_musicas_no_historico(vari VARCHAR(255))
-RETURNS INT READS SQL DATA
+CREATE FUNCTION quantidade_musicas_no_historico(vari INT)
+RETURNS INT DETERMINISTIC
 BEGIN
 DECLARE n INT;
 SELECT COUNT(*) INTO n FROM SpotifyClone.user_history AS uh
 INNER JOIN SpotifyClone.user AS u
 ON uh.user_id = u.user_id
-WHERE user = vari;
+WHERE u.user_id = vari;
 RETURN n;
 END $$
 
