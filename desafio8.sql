0077f2583aee8db9921007b5dc1b46f515d0edf3
nato-x <alvares.renato@hotmail.com>
2020-07-31 19:21:34 -0300

    D8 + agora vai

--- desafio8.sql
+++ desafio8.sql
@@ -1,12 +1,13 @@
 DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete 
- BEFORE DELETE ON SpotifyClone.users 
- FOR EACH ROW BEGIN DELETE FROM 
- SpotifyClone.following_artists 
- WHERE user_id = OLD.id;
-DELETE FROM SpotifyClone.play_history 
-WHERE
+  BEFORE DELETE ON SpotifyClone.users 
+  FOR EACH ROW BEGIN 
+  DELETE FROM 
+  SpotifyClone.following_artists 
+  WHERE user_id = OLD.id;
+  DELETE FROM SpotifyClone.play_history 
+  WHERE
     user_id = OLD.id;
- END $$
+  END $$
 
 DELIMITER ;
