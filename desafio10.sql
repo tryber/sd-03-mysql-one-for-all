efea9b4492f29c8c87ec39221658d25a7f32c487
clayton Pereira <cpereiramt@gmail.com>
2020-07-31 11:01:54 -0400

    Salvando alterações no desafio 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,14 +1,9 @@
 DELIMITER $$
-CREATE FUNCTION  quantidade_musicas_no_historico (nome VARCHAR(35))
+CREATE FUNCTION  quantidade_musicas_no_historico (id INT)
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE musicas INT ;
-SELECT COUNT(*)
-FROM historico_reproducao hr 
-INNER JOIN usuarios us
-ON us.usuario_id = hr.usuario_id
-WHERE
-us.usuario = nome INTO musicas;
+SELECT COUNT(*) FROM historico_reproducao hr WHERE hr.usuario_id = id INTO musicas;
 RETURN musicas;
 END $$
 DELIMITER ;
