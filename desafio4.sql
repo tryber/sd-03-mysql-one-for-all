71eb7547f6e257a597258faffcd89c5a5b1ce74a
clayton Pereira <cpereiramt@gmail.com>
2020-07-30 20:20:25 -0400

    Salvando alterações no desafio 04

---
+++ desafio4.sql
@@ -0,0 +1,9 @@
+CREATE VIEW `top_3_artistas` 
+AS SELECT a.artista_name, 
+COUNT(sa.artista_id) As `Seguidores`
+FROM seguindo_artista sa
+INNER JOIN artista a
+ON a.artista_id = sa.artista_id
+GROUP BY (sa.artista_id)
+ORDER BY 2 DESC, 1
+LIMIT 3
