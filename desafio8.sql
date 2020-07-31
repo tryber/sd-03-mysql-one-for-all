3129bc6d0310c41601ac6eb70127b9efc800b85d
Jafet Henrique <jafetguerra6@gmail.com>
2020-07-31 10:40:40 -0300

    corrigindo req 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,7 +1,7 @@
 USE SpotifyClone;
 DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete
-AFTER DELETE ON SpotifyClone.users
+BEFORE DELETE ON SpotifyClone.users
 FOR EACH ROW
 BEGIN
 DELETE FROM SpotifyClone.users_artists
