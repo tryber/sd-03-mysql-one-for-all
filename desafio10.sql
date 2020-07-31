7f2811b3073fa116a06542bc55af97196a3f0da7
Marco Barbosa <marco.meireles.b@gmail.com>
2020-07-31 15:06:07 -0300

    desafio10

---
+++ desafio10.sql
@@ -0,0 +1,15 @@
+USE SpotifyClone;
+
+DELIMITER $$
+
+CREATE FUNCTION quantidade_musicas_no_historico (user_id INT)
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE qtd INT;
+SELECT COUNT(song_id)
+FROM Reproduction_History
+WHERE Reproduction_history.user_id = user_id INTO qtd;
+RETURN qtd;
+END $$
+
+DELIMITER ;
