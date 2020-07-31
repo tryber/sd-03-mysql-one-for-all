9f64761abc1ab99774fd636cc5c78c4b50ecff0c
Jafet Henrique <jafetguerra6@gmail.com>
2020-07-31 10:28:34 -0300

    requisitos corrigidos

--- desafio9.sql
+++ desafio9.sql
@@ -2,8 +2,8 @@ DELIMITER $$
 CREATE PROCEDURE albuns_do_artista (IN nome_artista VARCHAR(30))
 BEGIN
 SELECT
-A.artist,
-AL.album 
+A.artist AS artista,
+AL.album  AS album
 FROM SpotifyClone.albuns AS AL
 INNER JOIN SpotifyClone.artists AS A
 ON A.artist_id = AL.artist_id
