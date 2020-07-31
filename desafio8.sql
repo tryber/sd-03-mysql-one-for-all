f80efa770d9127a78b892de2f7c0cfda5c57e678
nato-x <alvares.renato@hotmail.com>
2020-07-31 19:14:57 -0300

    2xD8 + DEX

---
+++ desafio8.sql
@@ -0,0 +1,10 @@
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete 
+ BEFORE DELETE ON SpotifyClone.users 
+ FOR EACH ROW BEGIN DELETE FROM 
+ SpotifyClone.following_artists 
+ WHERE user_id = OLD.id; 
+ DELETE FROM SpotifyClone.play_history  WHERE user_id = OLD.id;
+ END $$
+
+DELIMITER ;
