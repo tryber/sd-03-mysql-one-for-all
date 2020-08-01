dddee883b8257245be445cd1ac025ad451076fb0
rsfdev <roger_faustino@hotmail.com>
2020-08-01 17:49:07 -0300

    One for all project

---
+++ desafio4.sql
@@ -0,0 +1,5 @@
+CREATE VIEW top_3_artistas AS
+SELECT
+(SELECT name FROM SpotifyClone.artist AS artist WHERE artist.id = follow.artist_id) AS artista,
+COUNT(*) AS seguidores FROM SpotifyClone.follow AS follow
+GROUP BY artista ORDER BY seguidores DESC, artista LIMIT 3;
---
+++ desafio6.sql
@@ -0,0 +1,7 @@
+CREATE VIEW faturamento_atual AS
+SELECT
+ROUND(MIN(plan.value), 2) AS faturamento_minimo,
+ROUND(MAX(plan.value), 2) AS faturamento_maximo,
+ROUND(AVG(plan.value), 2) AS faturamento_medio,
+ROUND(SUM(plan.value), 2) AS faturamento_total
+FROM SpotifyClone.plan AS plan INNER JOIN SpotifyClone.user AS user ON plan.id = user.plan_id;
