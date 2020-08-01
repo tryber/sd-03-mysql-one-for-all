5c057146c032059ac12b79b3e389bea4874f0473
nato-x <alvares.renato@hotmail.com>
2020-07-31 23:23:49 -0300

    D10?

---
+++ desafio10.sql
@@ -0,0 +1,13 @@
+DELIMITER $$
+CREATE FUNCTION quantidade_musicas_no_historico(
+    user_name VARCHAR(30)
+)
+RETURNS INT READS SQL DATA
+BEGIN
+    DECLARE qt_song_history INT;
+    SELECT COUNT(user_id) FROM SpotifyClone.play_history
+    WHERE user_id LIKE (
+        SELECT name FROM SpotifyClone.users WHERE name LIKE user_name)
+        INTO qt_song_history;
+	RETURN qt_empregados;
+END $$
