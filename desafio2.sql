6da204cf8fc24fea4eb3487e2c49cc32f783ae35
rsfdev <roger_faustino@hotmail.com>
2020-08-01 18:14:56 -0300

    Project

--- desafio2.sql
+++ desafio2.sql
@@ -1,9 +1,7 @@
 CREATE VIEW estatisticas_musicais AS
   SELECT 
       COUNT(cancoes.id) AS 'cancoes',
-      (SELECT 
-              COUNT(artist.id) FROM SpotifyClone.artist AS artista) AS 'artistas',
-      (SELECT 
-              COUNT(album.id) FROM SpotifyClone.album AS album) AS 'albuns'
+      (SELECT COUNT(artist.id) FROM SpotifyClone.artist AS artista) AS 'artistas',
+      (SELECT COUNT(album.id) FROM SpotifyClone.album AS album) AS 'albuns'
     FROM
       SpotifyClone.songs AS cancoes;
