4dc0a7cc58c810a653cfe8e1118d74be2ffb671d
Gabryel <gabryel.ryba@gmail.com>
2020-07-31 22:54:16 -0300

    fixed identation and added newline at the end of the files

--- desafio2.sql
+++ desafio2.sql
@@ -1,5 +1,5 @@
 CREATE VIEW estatisticas_musicais AS
-	SELECT COUNT(id) AS cancoes,
-	(SELECT COUNT(id) FROM SpotifyClone.artist) AS artistas,
-	(SELECT COUNT(id) FROM SpotifyClone.album) AS albuns
-	FROM SpotifyClone.songs;
+SELECT COUNT(id) AS cancoes,
+(SELECT COUNT(id) FROM SpotifyClone.artist) AS artistas,
+(SELECT COUNT(id) FROM SpotifyClone.album) AS albuns
+FROM SpotifyClone.songs;
