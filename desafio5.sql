9b1cc4d78d8a431ff5275c2fa2a4650fa9aad751
rsfdev <roger_faustino@hotmail.com>
2020-08-02 12:26:38 -0300

    Project

--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,6 @@
 CREATE VIEW top_2_hits_do_momento AS
-SELECT cancoes.nome AS 'cancao', COUNT(historico.cancao_id) 'reproducoes'
-FROM SpotifyClone.historico AS historico
-INNER JOIN SpotifyClone.cancoes AS cancoes ON historico.cancao_id = cancoes.id
-GROUP BY (historico.cancao_id)
-ORDER BY 2 DESC, 1
-LIMIT 2;
+SELECT songs.name AS 'cancao', COUNT(history.song_id) 'reproducoes'
+FROM SpotifyClone.history AS historico
+INNER JOIN SpotifyClone.songs AS cancoes ON history.song_id = cancoes.id
+GROUP BY (history.song_id)
+ORDER BY 2 DESC, 1 LIMIT 2;
