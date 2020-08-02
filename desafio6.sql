d4daf445d4c28da5f9a61415667745e8034d0a0c
Lucas Franca <luc.rangel90@gmail.com>
2020-08-02 05:09:23 -0300

    cc

--- desafio6.sql
+++ desafio6.sql
@@ -4,5 +4,5 @@ MAX(plans.price) AS `faturamento_maximo`,
 ROUND(AVG(plans.price), 2) AS `faturamento_medio`,
 SUM(plans.price) AS `faturamento_total`
 FROM SpotifyClone.plans
-INNER JOIN SpotifyClone.users AS users
+JOIN SpotifyClone.users AS users
 ON users.plan_id = plans.id;
