9308ae3762e985300e25fcb055d890c1d02e6d18
nato-x <alvares.renato@hotmail.com>
2020-08-02 13:14:20 -0300

    d9 + D10 FIX

--- desafio10.sql
+++ desafio10.sql
@@ -1,3 +1,4 @@
+USE SpotifyClone;
 DELIMITER $$
 CREATE FUNCTION quantidade_musicas_no_historico(
     user_name VARCHAR(30)
@@ -6,8 +7,8 @@ RETURNS INT READS SQL DATA
 BEGIN
     DECLARE qt_song_history INT;
     SELECT COUNT(user_id) FROM SpotifyClone.play_history
-    WHERE user_id LIKE (
-        SELECT name FROM SpotifyClone.users WHERE name LIKE user_name)
+    WHERE user_id = (
+        SELECT id FROM SpotifyClone.users WHERE name LIKE user_name)
         INTO qt_song_history;
 RETURN qt_song_history;
 END $$
