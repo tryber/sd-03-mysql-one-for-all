030d031bbc64c1230cbaefab83245e515b155a2c
rsfdev <roger_faustino@hotmail.com>
2020-08-02 13:12:00 -0300

    Project

--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,6 @@
 CREATE VIEW perfil_artistas AS
-SELECT artista.nome AS 'artista', album.name AS 'album', COUNT(seguindo_artista.artista_id) AS 'seguidores'
-FROM SpotifyClone.artista AS artista
-INNER JOIN SpotifyClone.album AS album ON album.artista_id = artista.id
-INNER JOIN SpotifyClone.following_artist AS seguindo_artista ON seguindo_artista.artista_id = artista.id
+SELECT artista.name AS 'artista', album.name AS 'album', COUNT(seguindo_artista.artista_id) AS 'seguidores'
+FROM SpotifyClone.artist AS artista
+INNER JOIN SpotifyClone.album AS album ON album.artist_id = artista.id
+INNER JOIN SpotifyClone.following_artist AS seguindo_artista ON seguindo_artista.artist_id = artista.id
 GROUP BY (album.id) ORDER BY 3 DESC, 1, 2;
