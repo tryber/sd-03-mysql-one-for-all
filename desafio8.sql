531421237c5b279540f849111cea005c86e26cbb
Cardoso <lsc.cardosoo@gmail.com>
2020-08-02 15:32:40 -0300

    cc

--- desafio8.sql
+++ desafio8.sql
@@ -1,11 +1,11 @@
 DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete
-	BEFORE DELETE ON usuarios
-	FOR EACH ROW
+BEFORE DELETE ON usuarios
+FOR EACH ROW
 BEGIN
-	DELETE FROM seguindo_artistas
-  WHERE usuario_id = OLD.id;
-  DELETE FROM historico_usuario
-  WHERE usuario_id = OLD.id;
+DELETE FROM seguindo_artistas
+WHERE usuario_id = OLD.id;
+DELETE FROM historico_usuario
+WHERE usuario_id = OLD.id;
 END; $$
 DELIMITER $$;
