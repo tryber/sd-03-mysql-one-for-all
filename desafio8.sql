96699423ac8205de3bbd31bd88310606e5bf9e89
Cardoso <lsc.cardosoo@gmail.com>
2020-08-02 15:13:45 -0300

    Desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+	BEFORE DELETE ON usuarios
+	FOR EACH ROW
+BEGIN
+	DELETE FROM seguindo_artistas
+  WHERE usuario_id = OLD.id;
+  DELETE FROM historico_usuario
+  WHERE usuario_id = OLD.id;
+END; $$
+DELIMITER $$;
