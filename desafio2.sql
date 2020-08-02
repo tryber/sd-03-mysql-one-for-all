76fa2d9e054e62010d15e073427132ff5e5f5794
rsfdev <roger_faustino@hotmail.com>
2020-08-02 13:53:40 -0300

    Project

--- desafio2.sql
+++ desafio2.sql
@@ -1,7 +1,7 @@
 CREATE VIEW estatisticas_musicais AS
   SELECT 
       COUNT(cancoes.id) AS 'cancoes',
-      (SELECT COUNT(artist.id) FROM SpotifyClone.artist AS artista) AS 'artistas',
+      (SELECT COUNT(artista.id) FROM SpotifyClone.artista AS artista) AS 'artistas',
       (SELECT COUNT(album.id) FROM SpotifyClone.album AS album) AS 'albuns'
     FROM
-      SpotifyClone.songs AS cancoes;
+      SpotifyClone.cancoes AS cancoes;
--- desafio3.sql
+++ desafio3.sql
@@ -1,6 +1,6 @@
 CREATE VIEW historico_reproducao_usuarios AS
 SELECT
-(SELECT usuario.name FROM SpotifyClone.user WHERE user.id = re.user_id) AS usuario,
-(SELECT song.name FROM SpotifyClone.song WHERE song.id = re.song_id) AS nome
+(SELECT usuario.nome FROM SpotifyClone.usuario WHERE user.id = re.user_id) AS usuario,
+(SELECT song.nome FROM SpotifyClone.song WHERE song.id = re.song_id) AS nome
 FROM SpotifyClone.reproduction AS re
 ORDER BY 1, 2;
