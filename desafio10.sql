1144618d2f2dfcc6d5606548154eaf91534c8ea2
rsfdev <roger_faustino@hotmail.com>
2020-08-03 10:10:36 -0300

    Project

--- desafio10.sql
+++ desafio10.sql
@@ -5,9 +5,9 @@ DELIMITER $$
 CREATE FUNCTION quantidade_musicas_no_historico (user_id_param INT)
 RETURNS INT READS SQL DATA
 BEGIN
-DECLARE quant INT;
-SELECT COUNT(*) FROM history WHERE user_id = user_id_param INTO quant;
-RETURN quant;
+DECLARE qty INT;
+SELECT COUNT(*) FROM historico WHERE usuario_id = user_id_param INTO qty;
+RETURN qty;
 END $$
 
 DELIMITER ;
