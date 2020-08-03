07cf216ef5a82b0f54add63276c82f256da1de66
rsfdev <roger_faustino@hotmail.com>
2020-08-03 10:27:20 -0300

    Project

--- desafio11.sql
+++ desafio11.sql
@@ -1,14 +1,8 @@
-USE SpotifyClone;
-
-DELIMITER $$
-
 CREATE VIEW cancoes_premium AS
 SELECT cancoes.nome AS 'nome', COUNT(historico.cancao_id) AS 'reproducoes'
-  FROM historico AS historico
-    INNER JOIN cancoes AS cancoes ON cancoes.id = historico.cancao_id
-    INNER JOIN usuario AS usuario ON usuario.id = historico.usuario_id
-    WHERE usuario.plano_id IN (2, 3)
-  GROUP BY (cancoes.nome)
-  ORDER BY 1;
-
-DELIMITER ;
+FROM SpotifyClone.historico AS historico
+INNER JOIN SpotifyClone.cancoes AS cancoes ON cancoes.id = historico.cancao_id
+INNER JOIN SpotifyClone.usuario AS usuario ON usuario.id = historico.usuario_id
+WHERE usuario.plano_id IN (2, 3)
+GROUP BY (cancoes.nome)
+ORDER BY 1;
--- desafio7.sql
+++ desafio7.sql
@@ -1,5 +1,7 @@
 CREATE VIEW perfil_artistas AS
   SELECT artista.name AS 'artista', album.name AS 'album', COUNT(artists.artista_id) AS 'seguidores'
-    FROM SpotifyClone.artista AS artista INNER JOIN SpotifyClone.album AS album ON album.artista_id = artista.id
-      INNER JOIN SpotifyClone.artists AS artists ON artists.artista_id = artista.id
-  GROUP BY (album.id) ORDER BY 3 DESC, 1, 2;
+    FROM SpotifyClone.artista AS artista
+    INNER JOIN SpotifyClone.album AS album ON album.artista_id = artista.id
+    INNER JOIN SpotifyClone.artists AS artists ON artists.artista_id = artista.id
+  GROUP BY (album.id)
+  ORDER BY seguidores DESC, artista, album;
