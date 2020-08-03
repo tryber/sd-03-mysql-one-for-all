1edd891c694c3a3f1e1a3a1f2b881634f76e2673
rsfdev <roger_faustino@hotmail.com>
2020-08-03 09:40:37 -0300

    Project

--- desafio4.sql
+++ desafio4.sql
@@ -1,7 +1,7 @@
 CREATE VIEW top_3_artistas AS
-SELECT artista.name AS 'artista', COUNT(seguindo_artista.artist_id) AS 'seguidores'
-FROM SpotifyClone.following_artist AS seguindo_artista
-INNER JOIN SpotifyClone.artist AS artista ON seguindo_artista.artist_id = artista.id
-GROUP BY (seguindo_artista.artist_id)
+SELECT artista.name AS 'artista', COUNT(artists.artist_id) AS 'seguidores'
+FROM SpotifyClone.following_artist AS artists
+INNER JOIN SpotifyClone.artista AS artista ON artists.artist_id = artista.id
+GROUP BY (artists.artist_id)
 ORDER BY 2 DESC, 1
 LIMIT 3;
