47f71666fc897a7173adcd0a49af5c2cf2cc341d
luldelrey <rpgstrikered@gmail.com>
2020-08-03 20:41:36 -0300

    desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
 CREATE VIEW faturamento_atual AS
   SELECT FORMAT(MIN(up.valor), 2) AS faturamento_minimo,
-  MAX(up.valor) AS faturamento_maximo,
+  FORMAT(MAX(up.valor), 2) AS faturamento_maximo,
   FORMAT(ROUND(AVG(up.valor), 1), 2) AS faturamento_medio,
-  ROUND(SUM(up.valor), 2) AS faturamento_total
+  FORMAT(SUM(up.valor), 2) AS faturamento_total
   FROM SpotifyClone.user_table ut
   INNER JOIN SpotifyClone.user_plan up
   ON ut.plano = up.plano_id;
