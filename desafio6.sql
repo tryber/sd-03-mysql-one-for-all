32bcc0de969e62bc0ef22cc1bbf262c1f040e09a
luldelrey <rpgstrikered@gmail.com>
2020-08-03 20:20:12 -0300

    desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,8 @@
-SELECT MIN(up.valor) AS faturamento_minimo,
-MAX(up.valor) AS faturamento_maximo,
-FORMAT(ROUND(AVG(up.valor), 1), 2) AS faturamento_medio,
-ROUND(SUM(up.valor), 2) AS faturamento_total
-FROM SpotifyClone.user_table ut
-INNER JOIN SpotifyClone.user_plan up
-ON ut.plano = up.plano_id;
+CREATE VIEW faturamento_atual AS
+  SELECT MIN(up.valor) AS faturamento_minimo,
+  MAX(up.valor) AS faturamento_maximo,
+  FORMAT(ROUND(AVG(up.valor), 1), 2) AS faturamento_medio,
+  ROUND(SUM(up.valor), 2) AS faturamento_total
+  FROM SpotifyClone.user_table ut
+  INNER JOIN SpotifyClone.user_plan up
+  ON ut.plano = up.plano_id;
