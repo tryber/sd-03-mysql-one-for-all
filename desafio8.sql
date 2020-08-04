3fc16b44a83cefeaab1c33f49647ba9738524472
luldelrey <rpgstrikered@gmail.com>
2020-08-03 23:35:02 -0300

    desafio 8

--- desafio8.sql
+++ desafio8.sql
@@ -5,14 +5,10 @@ CREATE TRIGGER trigger_usuario_delete
     BEFORE DELETE ON user_table
     FOR EACH ROW
 BEGIN
-    DELETE * FROM user_follows
-    INNER JOIN user_table
-    ON user_table.user_id = user_follows.usuario
-    WHERE user_table.user_id = user_follows.usuario;
+    DELETE FROM user_follows
+    WHERE usuario = OLD.user_id;
 
-    DELETE * FROM user_history
-    INNER JOIN user_table
-    ON user_table.user_id = user_history.usuario
-    WHERE user_table.user_id = user_history.usuario;
+    DELETE FROM user_history
+    WHERE usuario = OLD.user_id;
 END; $$
 DELIMITER ;
