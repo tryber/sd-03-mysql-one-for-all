501d16f4a8e10dbe7d7a2e4b55f3f29170d11bf5
luldelrey <rpgstrikered@gmail.com>
2020-08-03 23:46:01 -0300

    desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,13 @@
+USE SpotifyClone;
+DELIMITER $$
+
+CREATE PROCEDURE albuns_do_artista(IN name CHAR(10))
+BEGIN
+  SELECT art.nome AS artista,
+  alb.nome AS album
+  FROM artist_table art
+  INNER JOIN album_table alb
+  ON art.artist_id = alb.artista
+  WHERE alb.nome = name;
+END $$
+DELIMITER ;
