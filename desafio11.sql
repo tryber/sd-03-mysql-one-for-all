87999cf466d276a2df57f27e834d86cf27777f61
luldelrey <rpgstrikered@gmail.com>
2020-08-04 00:23:00 -0300

    desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,9 +1,10 @@
-SELECT song AS nome,
-COUNT(his.usuario) AS reproducoes
-FROM user_history his
-INNER JOIN songs_table song
-ON his.cancao = song.song_id
-INNER JOIN user_table ut
-on ut.user_id = his.usuario AND ut.plano <> 1
-GROUP BY song
-ORDER BY 1 ASC;
+CREATE VIEW cancoes_premium AS
+  SELECT song AS nome,
+  COUNT(his.usuario) AS reproducoes
+  FROM user_history his
+  INNER JOIN songs_table song
+  ON his.cancao = song.song_id
+  INNER JOIN user_table ut
+  on ut.user_id = his.usuario AND ut.plano <> 1
+  GROUP BY song
+  ORDER BY 1 ASC;
