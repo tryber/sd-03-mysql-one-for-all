1e86817afae1970af0e3cb3ca8e1b14eca71282f
tamagoshii <estelaap_martins@hotmail.com>
2020-08-07 17:40:32 -0300

    desafio11

---
+++ desafio11.sql
@@ -0,0 +1,11 @@
+CREATE VIEW cancoes_premium AS
+    SELECT 
+        A.song AS nome, COUNT(user.id) AS reproducoes
+    FROM
+        SpotifyClone.song_history
+            INNER JOIN
+        songs AS song ON A.id = song_id
+            INNER JOIN
+        users AS user ON user_id = user.id AND user.plan_id <> 1
+    GROUP BY nome
+    ORDER BY nome;
