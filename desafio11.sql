9901f61b7e352c6d209a27a1163cdc82ee821adc
tamagoshii <estelaap_martins@hotmail.com>
2020-08-08 23:58:12 -0300

    Correcao desafio11

--- desafio11.sql
+++ desafio11.sql
@@ -1,11 +1,10 @@
-CREATE VIEW cancoes_premium AS
-    SELECT 
-        song.name AS nome, COUNT(user.id) AS reproducoes
-    FROM
-        SpotifyClone.song_history
-            INNER JOIN
-        songs AS song ON song.id = song_id
-            INNER JOIN
-        users AS user ON user_id = user.id AND user.plan_id <> 1
-    GROUP BY nome
-    ORDER BY nome;
+CREATE VIEW cancoes_premium AS SELECT 
+    song.name AS nome, COUNT(user.id) AS reproducoes
+FROM
+    SpotifyClone.song_history
+        INNER JOIN
+    songs AS song ON song.id = song_id
+        INNER JOIN
+    users AS user ON user_id = user.id AND user.plan_id <> 1
+GROUP BY nome
+ORDER BY nome;
