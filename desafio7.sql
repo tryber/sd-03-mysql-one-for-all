2911ea3817f6e04e8418e29c92e85e1ddb92ba10
tamagoshii <estelaap_martins@hotmail.com>
2020-08-09 02:33:25 -0300

    Desafio7

--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,7 @@
+USE SpotifyClone;
 CREATE VIEW perfil_artistas AS
     SELECT 
-        art.nome_artista artista,
+        art.artist artista,
         alb.album album,
         COUNT(fol_art.artist_id) seguidores
     FROM
@@ -8,7 +9,7 @@ CREATE VIEW perfil_artistas AS
         SpotifyClone.artists art,
         SpotifyClone.following_artists fol_art
     WHERE
-        alb.artists_id = art.artists_id
+        alb.artist_id = art.id
             AND fol_art.artist_id = alb.artist_id
-    GROUP BY alb.album_id
+    GROUP BY alb.id
     ORDER BY seguidores DESC , artista , album;
