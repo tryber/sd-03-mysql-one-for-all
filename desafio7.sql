e2bb1abe8802dfb9c660f7e400e38cf8fbe88de0
tamagoshii <estelaap_martins@hotmail.com>
2020-08-09 02:07:55 -0300

    D7

---
+++ desafio7.sql
@@ -0,0 +1,14 @@
+CREATE VIEW perfil_artistas AS
+    SELECT 
+        art.nome_artista artista,
+        alb.album album,
+        COUNT(fol_art.artist_id) seguidores
+    FROM
+        SpotifyClone.albuns alb,
+        SpotifyClone.artists art,
+        SpotifyClone.following_artist fol_art
+    WHERE
+        alb.artista_id = art.artista_id
+            AND fol_art.artist_id = alb.artist_id
+    GROUP BY alb.album_id
+    ORDER BY seguidores DESC , artista , album;
