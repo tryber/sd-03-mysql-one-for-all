5c5cbb7c6cb4d2e6b2e698564967f7edce431f73
Mateus Moreira <ashuraw@hotmail.com>
2020-08-10 16:11:13 -0300

    push complete

---
+++ desafio4.sql
@@ -0,0 +1,16 @@
+-- 4. Crie uma VIEW com o nome top_3_artistas que deve mostrar somente as três pessoas artistas mais populares no banco SpotifyClone, possuindo as seguintes colunas:
+
+-- A primeira coluna deve possuir o alias "artista" e exibir o nome da pessoa artista.
+-- A segunda coluna deve ter o alias "seguidores" e exibir a quantidade de pessoas que estão seguindo aquela pessoa artista.
+-- Seu resultado deve estar ordenado em ordem decrescente, baseando-se na quantidade de seguidores. Em caso de empate, ordene os resultados pelo nome da pessoa artista em ordem alfabética.
+
+CREATE VIEW top_3_artistas AS
+SELECT
+(SELECT artist_name FROM artists WHERE artists.id = followers.artist_id) AS artista,
+COUNT(*) AS seguidores
+FROM followers
+GROUP BY artista
+ORDER BY seguidores DESC, artista
+LIMIT 3;
+
+-- SELECT * FROM top_3_artistas;
