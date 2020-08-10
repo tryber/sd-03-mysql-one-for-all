79fcdb678ee3498da3d35a04e4620671a28da391
tamagoshii <estelaap_martins@hotmail.com>
2020-08-09 22:50:04 -0300

    vamogit add .

--- desafio9.sql
+++ desafio9.sql
@@ -6,7 +6,7 @@ A.artist AS artista,
 B.album  AS album
 FROM SpotifyClone.albuns AS B
 INNER JOIN SpotifyClone.artists AS A
-ON A.id = B.artist_id
-WHERE artist;
+ON A.artist = B.artist_id
+WHERE artist ;
 END $$
 DELIMITER ;
